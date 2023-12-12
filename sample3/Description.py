259b48779432fc02e666e392b35034d80517c968
Seung Tae LEE <129585999+SeungTaeGit@users.noreply.github.com>
2023-12-13 00:36:05 +0900

    Add files via upload

---
+++ sample3/Description.py
@@ -0,0 +1,52 @@
+import pygame
+import sys
+import os
+from pygame.locals import QUIT, KEYDOWN, K_UP,K_DOWN,K_SPACE,K_a,Rect
+import tkinter as tk
+from tkinter import messagebox
+from PIL import Image, ImageTk
+
+
+# 첫 번째 어려운 문제
+def Des():
+
+    root = tk.Tk()
+    root.title("Description")
+    root.geometry("1200x600")
+
+    image_path = "school.jpg"
+    original_image = Image.open(image_path)
+    resized_image = original_image.resize((1200, 600))
+    root.bg_photo = ImageTk.PhotoImage(resized_image)
+
+    bg_label = tk.Label(root, image=root.bg_photo)
+    bg_label.pack(fill="both", expand=True)
+
+    description = (
+        "최단 경로 게임에 오신 것을 환영합니다!\n\n"
+        "게임 목표 : 집에서 학교까지 가는 최단 경로를 찾는 것입니다.\n"
+        "게임을 시작하면 맵 상에 여러 경로들이 나타날 것이며 \n"
+        "문제를 풀며 최적의 경로를 찾아야 합니다.\n\n"
+        "게임 시작 버튼을 눌러 학교로 빨리 가보세요!"
+    )
+
+    def start_game():
+        messagebox.showinfo("게임 시작", "게임을 시작합니다!\n\n최단 경로를 찾아 집에서 학교까지 안전하게 이동하세요.")
+
+    button_start = tk.Button(root, text="게임 시작", command=start_game, border=5, font=("Helvetica", 20))
+    button_start.place(x=870, y=200)
+
+    def show_rules():
+        messagebox.showinfo("게임 규칙 설명", description)
+
+    button_rules = tk.Button(root, text="게임 규칙", command=show_rules, border=5, font=("Helvetica", 20))
+    button_rules.place(x=870, y=300)
+
+    def exit_game():
+        messagebox.showinfo("게임 종료", "게임을 종료합니다!")
+        root.destroy()
+
+    button_exit = tk.Button(root, text="종료하기", command=exit_game, border=5, font=("Helvetica", 20))
+    button_exit.place(x=870, y=400)
+
+    root.mainloop()
---
+++ sample3/Main.py
@@ -0,0 +1,210 @@
+import pygame
+import sys
+import os
+import random
+from pygame.locals import QUIT, KEYDOWN, K_UP,K_DOWN,K_SPACE,K_a,Rect
+import tkinter as tk
+from tkinter import messagebox
+from Brainer import *
+from NoBrainer import *
+from Back import *
+from Description import *
+
+
+
+# 다익스크라 경로 답
+ANS = ['House', 'Salon', 'Market', 'Academy', 'Restaurant', 'Bank', 'School']
+p = 1
+p = int(p)
+result = 0
+
+# 파이게임 초기화
+pygame.init()
+
+# 화면 설정
+width, height = 800, 600
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("파이게임 시작 화면")
+
+
+
+# 색상 정의
+white = (255, 255, 255)
+black = (0, 0, 0)
+
+# 시작 화면 텍스트
+font = pygame.font.Font(None, 36)
+text = font.render("Press Spacebar to Start", True, black)
+text_rect = text.get_rect(center=(width // 2, height // 2))
+
+current_path = os.path.dirname(__file__)
+image_path = os.path.join(current_path, "image")
+
+# 두 번째 화면 이미지
+second_screen_image = pygame.image.load(os.path.join(image_path, "map.jpg"))
+scaled_width, scaled_height = 800, 600
+second_screen_image = pygame.transform.scale(second_screen_image, (scaled_width, scaled_height))
+second_screen_rect = second_screen_image.get_rect()
+
+player_pos_x = 402
+player_pos_y = 105
+
+rect_x, rect_y = width // 2, height // 2
+rect_width, rect_height = 60, 60
+
+
+
+def over(): 
+    pygame.quit()
+    sys.exit()
+
+
+# 이벤트 루프
+start_screen = True
+while start_screen:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            start_screen = False
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            start_screen = False  # 스페이스바를 누르면 시작 화면 종료
+
+    # 화면을 흰색으로 채우기
+    screen.fill(white)
+
+    # 시작 화면 텍스트 그리기
+    screen.blit(text, text_rect)
+
+    # 화면 업데이트
+    pygame.display.flip()
+
+
+Des()
+
+
+# 두 번째 화면 설정
+second_screen = True
+
+# 이벤트 루프 (두 번째 화면)
+while second_screen:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            second_screen = False
+
+
+    key_event = pygame.key.get_pressed()
+    if key_event[pygame.K_LEFT]:
+        player_pos_x -= 0.3
+    
+    if key_event[pygame.K_RIGHT]:
+        player_pos_x += 0.3
+
+    if key_event[pygame.K_UP]:
+        player_pos_y -= 0.3
+    
+    if key_event[pygame.K_DOWN]:
+        player_pos_y += 0.3
+
+    if key_event[pygame.K_a]:
+        current_x, current_y = player_pos_x, player_pos_y
+
+        # 미용실
+        if (rect_x - 320 <= current_x <= rect_x - 260) and (rect_y - 10 <= current_y <= rect_y + 50):
+            if (ANS[p] == 'Salon') :
+                messagebox.showinfo("경로 확인", "경로 선택 성공")
+                success()
+                result = back2()
+                p = p + result
+                print(p)
+                result = 0
+            else :
+                messagebox.showinfo("경로 확인", "경로 선택 실패")
+                fail()
+        # 슈퍼마켓
+        elif (rect_x - 40 <= current_x <= rect_x + 40) and (rect_y - 80 <= current_y <= rect_y - 20):
+            if (ANS[p] == 'Market') :
+                messagebox.showinfo("경로 확인", "경로 선택 성공")
+                success()
+                result = back2()
+                p = p + result
+                print(p)
+                result = 0
+            else :
+                messagebox.showinfo("경로 확인", "경로 선택 실패")
+                fail()
+        # 학원
+        elif (rect_x + 230 <= current_x <= rect_x + 300) and (rect_y - 110 <= current_y <= rect_y - 20):
+            if (ANS[p] == 'Academy') :
+                messagebox.showinfo("경로 확인", "경로 선택 성공")
+                success()
+                result = back2()
+                p = p + result
+                print(p)
+                result = 0
+            else :
+                messagebox.showinfo("경로 확인", "경로 선택 실패")
+                fail()
+        # 레스토랑
+        elif (rect_x - 140 <= current_x <= rect_x - 70) and (rect_y + 50 <= current_y <= rect_y + 110):
+            if (ANS[p] == 'Restaurant') :
+                messagebox.showinfo("경로 확인", "경로 선택 성공")
+                success()
+                result = back2()
+                p = p + result
+                print(p)
+                result = 0
+            else :
+                messagebox.showinfo("경로 확인", "경로 선택 실패")
+                fail()
+        # 은행
+        elif (rect_x - 30 <= current_x <= rect_x + 70) and (rect_y + 150 <= current_y <= rect_y + 230):
+            if (ANS[p] == 'Bank') :
+                messagebox.showinfo("경로 확인", "경로 선택 성공")
+                success()
+                result = back2()
+                p = p + result
+                print(p)
+                result = 0
+            else :
+                messagebox.showinfo("경로 확인", "경로 선택 실패")
+                fail()
+        # 학교
+        elif (rect_x + 190 <= current_x <= rect_x + 285) and (rect_y + 100 <= current_y <= rect_y + 180):
+            if (ANS[p] == 'School') :
+                messagebox.showinfo("경로 확인", "경로 선택 성공")
+                success()
+                result = back2()
+                p = p + result
+                print(p)
+                result = 0
+                if (p == 7):
+                    messagebox.showinfo("Game clear", "Congratulations !")
+                    pygame.quit()
+                    sys.exit()
+            else :
+                messagebox.showinfo("경로 확인", "경로 선택 실패")
+                fail()
+
+    # 화면에 이미지 표시
+    screen.blit(second_screen_image, second_screen_rect)
+
+    
+    # 노드 좌표
+    #pygame.draw.rect(screen, black, (rect_x - 320, rect_y - 10, rect_width, rect_height))              # 미용실
+    #pygame.draw.rect(screen, black, (rect_x - 40, rect_y - 80, rect_width + 20, rect_height))          # 슈퍼마켓
+    #pygame.draw.rect(screen, black, (rect_x + 230, rect_y - 110, rect_width + 10, rect_height + 30))   # 학원
+    #pygame.draw.rect(screen, black, (rect_x - 140, rect_y + 50, rect_width + 10, rect_height))         # 레스토랑
+    #pygame.draw.rect(screen, black, (rect_x - 30, rect_y + 150, rect_width + 10, rect_height + 20))    # 은행
+    #pygame.draw.rect(screen, black, (rect_x + 190, rect_y + 100, rect_width + 35, rect_height + 20))   # 학교
+
+    # 플레이어 좌표
+    pygame.draw.circle(screen, (255, 255, 255), (player_pos_x, player_pos_y), 15)
+    
+    # 화면 업데이트
+    pygame.display.flip()
+
+# 파이게임 종료
+pygame.quit()
+sys.exit()
+
